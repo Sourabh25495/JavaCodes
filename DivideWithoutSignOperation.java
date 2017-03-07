5adbbf01b80f17114a0a3ac0c29d153c6d27df35
sourabh kulkarni <skulkar6@stevens.edu>
2017-03-07 11:43:50 -0500

    Create DivideWithoutSignOperation.java

---
+++ DivideWithoutSignOperation.java
@@ -0,0 +1,41 @@
+public class Program {
+    public static void main(String[] args) {
+        assert divide(10, 4) == 2;
+        assert divide(11, 4) == 2;
+        assert divide(12, 4) == 3;
+        assert divide(13, 4) == 3;
+        assert divide(130,3 ) == 43;
+
+        boolean threw = false;
+        try {
+            divide(1, 0);
+        } catch (ArithmeticException ex) {
+            threw = true;
+        }
+        assert threw;
+
+        System.out.println("Tests passed");
+    }
+
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException(
+                    "Division by 0 is undefined: " + a + "/" + b);
+        }
+        int sign = 1;
+        if (a < 0) {
+            a = -a;
+            sign = -sign;
+        }
+        if (b < 0) {
+            b = -b;
+            sign = -sign;
+        }
+        double result = 0;
+        while (a >= 0) {
+            a -= b;
+            result++;
+        }
+        return (result - 1) * sign;
+    }
+}
